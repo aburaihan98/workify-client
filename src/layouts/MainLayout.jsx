d040809e2a18fc9784a27905a1a4333e481d828a
Abu Raihan <aburaihanrahmani@gmail.com>
2025-01-14 02:08:23 +0600

    Added Banner  with responsive design

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Banner from "../components/Home/Banner";
 import NavBar from "../components/Shared/Navbar/NavBar";
 
 const MainLayout = () => {
   return (
     <div className="bg-white">
       <NavBar />
-
+      <Banner />
       <div className=" min-h-[calc(100vh-68px)]">
         <Outlet />
       </div>
