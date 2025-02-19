5060c948c3c8070569aeaeeccbe18dd4c793e112
Abu Raihan <aburaihanrahmani@gmail.com>
2025-02-19 15:38:32 +0600

    Add navbar sticky mode

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -32,26 +32,23 @@ function ContactUs() {
   };
 
   return (
-    <div className=" min-h-screen py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-lg mx-auto bg-white p-8 rounded-lg shadow-lg">
+    <div className="min-h-screen py-6 md:py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-lg mx-auto p-4 md:p-8 rounded-lg shadow-lg">
         {/* Company Address */}
         <div className="text-center mb-8">
-          <h2 className="text-3xl font-semibold text-gray-800">Contact Us</h2>
-          <p className="mt-2 text-gray-600">We'd love to hear from you!</p>
+          <h2 className="text-3xl font-semibold">Contact Us</h2>
+          <p className="mt-2">We'd love to hear from you!</p>
         </div>
 
-        <div className="bg-gray-100 p-4 rounded-lg mb-8">
-          <h3 className="text-xl font-semibold text-gray-800">Our Company</h3>
-          <p className="mt-2 text-gray-600">1234 Street Name, City, Country</p>
+        <div className="p-4 rounded-lg mb-8">
+          <h3 className="text-xl font-semibold">Our Company</h3>
+          <p className="mt-2">1234 Street Name, City, Country</p>
         </div>
 
         {/* Contact Form */}
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
+            <label htmlFor="email" className="block text-sm font-medium ">
               Your Email
             </label>
             <input
@@ -61,15 +58,12 @@ function ContactUs() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
+              className="text-primary mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
             />
           </div>
 
           <div>
-            <label
-              htmlFor="message"
-              className="block text-sm font-medium text-gray-700"
-            >
+            <label htmlFor="message" className="block text-sm font-medium ">
               Your Message
             </label>
             <textarea
@@ -79,7 +73,7 @@ function ContactUs() {
               onChange={(e) => setMessage(e.target.value)}
               rows="4"
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
+              className="text-primary mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
             />
           </div>
 
